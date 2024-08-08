6f7d68d1332b96daa582bbb8293a04bd7797555e
yuanjun <eugenelau@foxmail.com>
2024-08-08 08:48:30 +0800

    add test suit

---
+++ src/test/java/org/away/MainTest.java
@@ -0,0 +1,15 @@
+package org.away;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+
+        System.out.println("hello");
+
+        Assert.assertTrue(true);
+    }
+}
